f1a1971cd68e5d0ac692d7e8d413e8966429dd7e
Ricardo <82323662+Luziferri@users.noreply.github.com>
2026-01-30 20:17:54 +0000

    Preparar para Vercel

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,4 @@ import react from '@vitejs/plugin-react'
 // [https://vitejs.dev/config/](https://vitejs.dev/config/)
 export default defineConfig({
   plugins: [react()],
-  // IMPORTANTE: O nome do repositório entre barras
-  base: '/meu-portfolio/', 
 })
